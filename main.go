5b4227d1a02a153e5459c2e0fb2b8905188fa08e
gzel <50979270+glangzel@users.noreply.github.com>
2022-02-01 13:43:35 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"dockerEdit/dockerEdit"
-	"flag"
-	"io/ioutil"
+	"DFGenerator/dockerEdit"
+	"fmt"
+	"os"
 )
 
 func main() {
-	f := flag.String("flag1", "hoge", "flag 1")
-	flag.Parse()
-	j, err := ioutil.ReadFile(*f)
-	//"/Users/hoge/fuga.json"
-	k := string(j)
-	dockerfile := dockerEdit.Djson(k)
-	dockerfile = dockerEdit.Dstring(dockerfile)
-	print(err)
-	print(dockerfile)
+	did := os.Args[1]
+	a, _ := dockerEdit.SaveImage(did)
+
+	j := dockerEdit.OpenTar(a, did)
+
+	fp, err2 := os.Create("dockerfile")
+	if err2 != nil {
+		fmt.Println(err2)
+		return
+	}
+	defer fp.Close()
+	//println(j)
+
+	var output []string
+	output = dockerEdit.Dunmarshal(j)
+	dockerEdit.Dstring(output, fp)
+
 }
