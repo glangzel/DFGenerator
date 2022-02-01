dd8c4458d938a2ebc23f2195f61117bd5924982b
gzel <50979270+glangzel@users.noreply.github.com>
2022-02-01 13:44:16 +0900

    Update dockerEdit.go

--- dockerEdit/dockerEdit.go
+++ dockerEdit/dockerEdit.go
@@ -1,28 +1,91 @@
 package dockerEdit
 
 import (
-	"bytes"
+	"archive/tar"
 	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 	"strings"
-	//"io/ioutil"
-	//"os"
-	//"log"
-	//"github.com/tidwall/gjson"
+
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
 )
 
-func Djson(beforeJ string) string {
-	//j, err := ioutil.ReadFile("docker_debian/5852ca.json")
-	var buf bytes.Buffer
-	err := json.Indent(&buf, []byte(beforeJ), "", "  ")
-	if err != nil {
-		panic(err)
-	}
-	indentJ := buf.String()
-	return indentJ
+type Key struct {
+	History []Key2 `json:"history"`
 }
 
-func Dstring(str string) string {
-	str_replaced := strings.Replace(str, "CMD", "Replaced", -1)
-	//同様にしてDockerfileと違う部分をどんどん置き換えていく
-	return str_replaced
+type Key2 struct {
+	Created_by string `json:"created_by"`
+}
+
+func Dunmarshal(str string) []string {
+	//fmt.Println(str)
+	b := []byte(str)
+	var k Key
+	if err := json.Unmarshal(b, &k); err != nil {
+		panic(err)
+	}
+	//fmt.Printf("%+v\n", k.History[0].Created_by)
+
+	var hist []string
+
+	for i := range k.History {
+		hist = append(hist, k.History[i].Created_by)
+	}
+	return hist
+}
+
+func Dstring(output []string, fp *os.File) {
+	for i := range output {
+		output[i] = strings.Replace(output[i], "/bin/sh -c #(nop)", "", -1)
+		output[i] = strings.Replace(output[i], "/bin/sh -c", "RUN", -1)
+		fp.WriteString(output[i])
+		fp.WriteString("\n")
+	}
+}
+
+func SaveImage(id string) (io.ReadCloser, error) {
+	var err error
+	var cli *client.Client
+
+	ctx := context.Background()
+
+	cli, err = client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return nil, err
+	}
+
+	readCloser, err := cli.ImageSave(ctx, []string{id})
+	if err != nil {
+		return nil, err
+	}
+
+	return readCloser, nil
+}
+
+func OpenTar(dimage io.ReadCloser, did string) string {
+	//buf := make([]byte, 1024)
+	var dfile []byte
+	file := dimage
+	defer file.Close()
+
+	// tarの展開
+	tarReader := tar.NewReader(file)
+	for {
+		tarHeader, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+
+		if tarHeader != nil {
+			if strings.Contains(tarHeader.Name, did) == true {
+				dfile, _ = ioutil.ReadAll(tarReader)
+				break
+			}
+		}
+	}
+	return string(dfile)
 }
